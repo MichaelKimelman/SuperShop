eb365995fe471244f3f9f2aa304e1e5ce1c6894d
stefanh <stefanh@dev01>
2021-07-19 14:34:29 +0000

    Add project files.

---
+++ StefansSuperShop/Data/DataInitializer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace StefansSuperShop.Data
+{
+    public class DataInitializer
+    {
+        public static void SeedData(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            dbContext.Database.Migrate();
+        }
+    }
+}
---
+++ StefansSuperShop/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using StefansSuperShop.Data;
+
+namespace StefansSuperShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+                DataInitializer.SeedData(dbContext, userManager);
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
