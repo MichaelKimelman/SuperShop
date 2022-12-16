5872066fa338d658b31dfa5d28c046c0e1cd4094
Rano Zangana <116091120+hazelrano@users.noreply.github.com>
2022-12-16 13:40:28 +0100

    La in lite smtp-grejer

---
+++ StefansSuperShop/Services/EmailSenderService.cs
@@ -0,0 +1,46 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+
+namespace StefansSuperShop.Services
+{
+    public class EmailSenderService : IEmailSenderService
+    {
+        private readonly MailSettings _settings;
+        public EmailSenderService(MailSettings mailSettings)
+        {
+            _settings = mailSettings;
+        }
+        public void SendEmail(string email, string name, string header, string message)
+        {
+            var mail = new MimeMessage();
+
+            mail.From.Add(new MailboxAddress(_settings.DisplayName, _settings.From));
+            mail.Sender = new MailboxAddress(_settings.DisplayName, _settings.From);
+
+            mail.To.Add(MailboxAddress.Parse(email));
+
+            var body = new BodyBuilder();
+            mail.Subject = header;
+            body.HtmlBody = message;
+            mail.Body = body.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            
+            if (_settings.UseSSL)
+            {
+                smtp.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.SslOnConnect).Wait();
+            }
+            else if (_settings.UseStartTls)
+            {
+                smtp.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.StartTls).Wait();
+            }
+            smtp.AuthenticateAsync(_settings.UserName, _settings.Password).Wait();
+            smtp.SendAsync(mail).Wait();
+            smtp.DisconnectAsync(true).Wait();
+
+        }
+    }
+}
---
+++ StefansSuperShop/Services/IEmailSenderService.cs
@@ -0,0 +1,7 @@
+﻿namespace StefansSuperShop.Services
+{
+    public interface IEmailSenderService
+    {
+        void SendEmail(string email, string name, string header, string message);
+    }
+}
