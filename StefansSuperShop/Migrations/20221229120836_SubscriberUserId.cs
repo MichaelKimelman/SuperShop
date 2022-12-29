6960f02e19d6b3c50cf18eb5dc92db9afa9e4fab
MichaelKimelman <91721421+MichaelKimelman@users.noreply.github.com>
2022-12-29 14:05:48 +0100

    Signup funktionalitet + User & UserId properties i Subscriber

---
+++ StefansSuperShop/Migrations/20221229120836_SubscriberUserId.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StefansSuperShop.Migrations
+{
+    public partial class SubscriberUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Subscribers",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscribers_UserId",
+                table: "Subscribers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscribers_AspNetUsers_UserId",
+                table: "Subscribers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscribers_AspNetUsers_UserId",
+                table: "Subscribers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subscribers_UserId",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Subscribers");
+        }
+    }
+}
---
+++ StefansSuperShop/Pages/Subscriber.cshtml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StefansSuperShop.Data;
+
+namespace StefansSuperShop.Pages
+{
+    public class SubscriberModel : PageModel
+    {
+        private readonly StefansSuperShop.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public SubscriberModel(StefansSuperShop.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        [BindProperty]
+        [EmailAddress]
+        public string Epost { get; set; }
+        public string Id { get; set; }
+
+        [BindProperty]
+        public Subscriber Subscriber { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Subscribers.Add(Subscriber);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+        public async Task<IActionResult> OnPostNewsletterLayoutAsync()
+        {
+            Subscriber model;
+            var user = _userManager.GetUserAsync(User).Result != null ? _userManager.GetUserAsync(User).Result : null;
+
+            //Kolla om epost är valid(Typ)
+            if (Epost == null)
+            {
+                return Page();
+                //return RedirectToPage("./Index");
+            }
+            
+            //Kolla om epost finns registrerad
+            if (!_context.Subscribers.ToList().Exists(x => x.Email == Epost))
+             {
+                model = new Subscriber
+                {
+                    Email = Epost,
+
+                };
+
+                
+            }
+            else
+            {
+                return Page();
+            }
+
+            //Checka om user är invalid
+            if (user != null)
+            {
+                model.User = user;
+                model.UserId = user.Id;
+            }
+            //Checka om user redan är registrerad(Bara tills Newsletter markup tas bort när inloggad och registrerad)
+            if(_context.Subscribers.ToList().Exists(x => x.UserId == user.Id))
+            {
+                return Page();
+            }
+
+
+            _context.Subscribers.Add(model);
+            await _context.SaveChangesAsync();
+
+            return Page();
+        }
+
+    }
+}
