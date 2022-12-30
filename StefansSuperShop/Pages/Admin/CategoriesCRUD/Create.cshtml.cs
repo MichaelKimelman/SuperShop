9deed72365f9a120eb7b69a2712fa2928875a795
MichaelKimelman <91721421+MichaelKimelman@users.noreply.github.com>
2022-12-30 15:01:56 +0100

    Admin: CategoryList + Category Edit & Create

---
+++ StefansSuperShop/Pages/Admin/CategoriesCRUD/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StefansSuperShop.Data;
+
+namespace StefansSuperShop.Pages.Admin.CategoriesCRUD
+{
+    public class CreateModel : PageModel
+    {
+        private readonly StefansSuperShop.Data.ApplicationDbContext _context;
+
+        public CreateModel(StefansSuperShop.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Categories Categories { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Categories.Add(Categories);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ StefansSuperShop/Pages/Admin/CategoriesCRUD/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StefansSuperShop.Data;
+
+namespace StefansSuperShop.Pages.Admin.CategoriesCRUD
+{
+    public class IndexModel : PageModel
+    {
+        private readonly StefansSuperShop.Data.ApplicationDbContext _context;
+        public IList<CategoryRow> Categories { get; set; }
+        public class CategoryRow
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
+        public IndexModel(StefansSuperShop.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Categories = await _context.Categories.Select(e => new CategoryRow
+            {
+                Id = e.CategoryId,
+                Name = e.CategoryName,
+                Description = e.Description,
+            }).ToListAsync();
+        }
+    }
+}
