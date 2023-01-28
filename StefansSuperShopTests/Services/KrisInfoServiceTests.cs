9feec72974fa4ff0730748919469485e1fdb8da2
DLindeberg <90319103+DLindeberg@users.noreply.github.com>
2023-01-28 16:52:34 +0100

    Added Test for Krisinfo

---
+++ StefansSuperShopTests/Services/KrisInfoServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using StefansSuperShop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StefansSuperShopTests.Services
+{
+    [TestClass]
+    public class KrisInfoServiceTests
+    {
+        private KrisInfoService sut;
+
+        public KrisInfoServiceTests()
+        {
+            sut = new KrisInfoService();
+        }
+
+        [TestMethod]
+        public void Test_if_API_returns_list()
+        {
+            var lista = sut.GetAllKrisInformation();
+
+            Assert.IsNotNull(lista);
+        }
+    }
+}
